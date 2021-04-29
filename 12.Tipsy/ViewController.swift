c12ed06e74f8f6e98f5ee058d737962924f0bc28
Heny <hyoh0606@gmail.com>
2021-04-30 02:06:27 +0900

    Initial Commit

---
+++ 12.Tipsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12.Tipsy
+//
+//  Created by 오현호 on 2021/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
